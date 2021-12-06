f8f7ec6d97978933cfe113c5ee063ab28a34526e
kathyavini <kathyavini@gmail.com>
2021-12-05 20:02:25 -0800

    Add game module, game logic, win states, win/tie message, player names from spans. No AI yet.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,150 +1,279 @@
 
-
-
-
-/* "Build the functions that allow players to add marks to 
-specific spot on the board (function: allowed marks), letting
-players click on the gameboard to place their marker. Don't
-forget the logic that keeps players from playing in spots already
-taken"
-
-- "Each piece of functionality should fit in the game, player,
-or gameboard objects"
-
-- "build logic that checks for when the game is over. Should check
-for 3-in-a-row and a tie"
-- Clean up interface to allow players to put in their names
-- Include a button to start/restart
-- Add a display element that congratulates the winning player
-
-AI instructions
-- Start by getting the computer to make a random legal move
-- You can make an unbeatable AI using minimax
-
-*/
-
-// Gameboard module
-
-// The Gameboard module will have rules for adjusting the array
-// For rendering the current array onto the page
-// For making sure that the preview corresponds to the player whose choice it is (X or 0)
-
+// Gameboard Module. Stores the board as an array, displays the array, checks for win states
+// Public methods: updateBoard, clearBoard
 let gameBoard = (function(){
     
     let board = [
         0, 0, 0, 
         0, 0, 0,
-        0, 0, 0];
-    
-    function printBoard() { // just for testing
-        console.log(board);
-    }
+        0, 0, 0
+    ];
 
-    function updateBoard(mark, position) {
-        board[position-1] = mark;
-    }
+
+    // CSS value strings
+    const x = 'no-repeat center/contain url("./img/x.svg"), #e1e6fb'
+    const o = 'no-repeat center/contain url("./img/o.svg"), #e1e6fb'
 
     function displayBoard() {
         for ([index, square] of board.entries()) {
-            targetSquare = document.querySelector(`main div:nth-child(${index + 1})`);
+            targetSquare = document.querySelector(`[data-square="${index + 1}"]`);
             if (square == 1) {
-                targetSquare.style.background = 
-                    'no-repeat center/contain url("./img/x.svg"), #e1e6fb';
+                targetSquare.style.background = x;
             } else if (square == 2) {
-                targetSquare.style.background = 
-                    'no-repeat center/contain url("./img/o.svg"), #e1e6fb';
+                targetSquare.style.background = o;
+            } else {
+                targetSquare.style.background = '';
             }
         }
     }
+
+    function clearBoard() {
+        for ([index, value] of board.entries()) {
+            board[index] = 0;
+        }
+        displayBoard();
+        updateAvailableMoves();
+    }
+
+    function updateAvailableMoves() {
+        for ([index, square] of board.entries()) {
+            targetSquare = document.querySelector(`[data-square="${index + 1}"]`);
+            if (square !== 0) {
+                targetSquare.style.pointerEvents = 'none';
+            } else {
+                targetSquare.style.pointerEvents = 'initial';
+            }
+        }
+    }
+
+    function updateBoard(mark, position) {
+        board[position - 1] = mark;
+        displayBoard();
+        updateAvailableMoves();
+        checkForGameEnd();
+    }
+
+    function checkForGameEnd() {
+        winString = checkWin();
+        tie = checkTie();
+        if (winString) {
+            game.endGame(winString);
+        } else if (tie) {
+            game.endGame('tie');
+        }
+    }
+        
+    function checkWin() {
+        // row wins
+        for (let i = 0; i < 9; i += 3) {
+            if (board[i] !== 0 &&
+                board[i] == board[i + 1] &&
+                board[i] == board[i + 2]) {
+                return winString = `[${i + 1}, ${i + 3}]`;
+            }
+        }
+        // column wins
+        for (let i = 0; i < 3; i++) {
+            if (board[i] !== 0 &&
+                board[i] == board[i + 3] && 
+                board[i] == board[i + 6]) {
+                return winString = `[${i + 1}, ${i + 7}]`;
+            }
+        }
+
+        // diagonal wins
+        if (board[0] !== 0 &&
+            board[0] == board[4] &&
+            board[0] == board[8]) {
+                return winString = '[1, 9]';
+        }
+
+        if (board[2] !== 0 &&
+            board[2] == board[4] &&
+            board[2] == board[6]) {
+                return winString = '[3, 7]';
+        }
+
+        return false;
+    }
+
+    function checkTie() {
+        if (board.includes(0)) {
+            return false
+        }
+        return true
+    }
     
     return {
-        printBoard,
         updateBoard,
-        displayBoard,
+        clearBoard,
     }
     
 })();
 
-
-// Game
-// Should check for the win state
-// Should display the correct win line
-// Should give the win message
-
-// I think I'll put the functionality for deciding who can play where
-// And which mark gets updated with each click here as well?
-
-// A little less than ideal because both gameBoard and game would end up with
-// a representation of the board... and should't the event listeners for the
-// board go into the gameBoard object? Something to think about...
-
-
-// The players objects???
-// Honestly I don't know yet why I would use these. But I 
-// need to practice making a factory function, so...
-
-function createPlayer(name) {
+// Game module. Event listeners for board, currentPlayer control, updating hover preview according to current player.
+// Public methods: addEventListeners, endGame
+let game = (function() {
     
-    let score = 0;
+    let currentPlayer = 1;
+    let hoverTiles = document.querySelectorAll('main div');
+    let resetButton = document.querySelector('button');
+    let root = document.querySelector(':root');
+    let winLine = document.querySelector('.win-line');
+    let resultsCard = document.querySelector('.result-card')
+    let board = document.querySelector('main');
 
-    function incrementScore() {
-        score++;
+    function addEventListeners() {
+        for (tile of hoverTiles) {
+            tile.addEventListener('click', placeMark);
+        }
+        resetButton.addEventListener('click', () => {
+            gameBoard.clearBoard();
+            if (currentPlayer == 2) {
+                changePlayer();
+            }
+            hideWinLine();
+            hideResults();
+        });
     }
 
-    function printScore() {
-        console.log(`${name} has ${score} points`)
+    function changePlayer() {
+        if (currentPlayer == 1) {
+            currentPlayer = 2;
+        } else {
+            currentPlayer = 1;
+        }
+        root.classList.toggle('player2');
+    }
+
+    function placeMark() {
+        if (currentPlayer == 1) {
+            gameBoard.updateBoard(1, this.dataset.square);
+        } else if (currentPlayer == 2) {
+            gameBoard.updateBoard(2, this.dataset.square);
+        }
+        changePlayer();
+    }
+
+    function showWinLine(type) {
+        moveWinLine(type)
+        winLine.classList.add('visible');
+    }
+
+    function moveWinLine(coordinates) { // as string '[start, end]'
+        switch (coordinates) {
+            case '[1, 9]': // diagonal
+                lineTransform = 'rotate(45deg) translate(-148px, -148px)';
+                break;
+            case '[4, 6]': // middle row
+                lineTransform = 'translateY(-210px)';
+                break;
+            case '[1, 3]': // top row
+                lineTransform = 'translateY(-315px)';
+                break;
+            case '[7, 9]': // bottom row
+                lineTransform = 'translateY(-105px)';
+                break;
+            case '[3, 7]': // diagonal
+                lineTransform = 'rotate(135deg) translate(-148px, 148px)';
+                break;
+            case '[1, 7]': // first column
+                lineTransform = 'translate(-105px, -205px) rotate(90deg)';
+                break;
+            case '[2, 8]': // second column
+                lineTransform = 'translate(0, -205px) rotate(90deg)';
+                break;
+            case '[3, 9]': // third column
+                /* X and Y transform switch if you rotate first*/
+                lineTransform = 'rotate(90deg) translate(-205px, -105px)';
+                break;
+        }
+        winLine.style.transform = `${lineTransform}`;
+    }
+
+    function hideWinLine() {
+        winLine.classList.remove('visible');
+    }
+
+    function endGame(type) {
+        for (tile of hoverTiles) {
+            tile.style.pointerEvents = 'none';
+        }
+
+        if (type === 'tie') {
+            showResults("It's a tie!");
+            return
+        }
+
+        showWinLine(type);
+
+        if (currentPlayer == 1) {
+            winner = player1.returnName();
+        } else {
+            winner = player2.returnName();
+        }
+
+        showResults(`${winner} wins!`);
+    }
+
+    function showResults(message) {
+        resultsCard.textContent = message;
+        resultsCard.classList.add("bring-front");
+        board.classList.add("blur");
+        winLine.classList.add("blur");
+    }
+
+    function hideResults() {
+        resultsCard.classList.remove("bring-front");
+        board.classList.remove("blur");
+        winLine.classList.remove("blur");
     }
 
     return {
-        incrementScore,
-        printScore,
+        addEventListeners,
+        endGame,
+    }
+})();
+
+
+// The players objects
+
+// Not so sure why I would use these
+// Currently they do nothing but store and display their names.
+// But I need to practice making a factory function, so...
+
+function createPlayer(playerNumber) {
+
+    let playerSpans = document.querySelectorAll('span');
+    let playerSpan = playerSpans[playerNumber - 1];
+    
+    // Currently loads as Human vs. Computer
+    // let playerName = playerSpan.textContent;
+
+    // If you prefer Player 1, Player 2:
+    let playerName = '';
+
+    playerSpan.addEventListener('blur', updateName);
+    
+    function updateName() {
+        playerName = playerSpan.textContent;
+        this.playerName = playerName;
+    }
+
+    function returnName() {
+        if (playerName) {
+            return playerName;
+        } else {
+            return `Player ${playerNumber}`;
+        } 
+    }
+
+    return {
+        returnName
     }
 }
 
-human = createPlayer('Human');
-computer = createPlayer('Computer');
+let player1 = createPlayer(1);
+let player2 = createPlayer(2);
 
-
-
-// Just for testing CSS
-grid = document.querySelector('main');
-grid.addEventListener('click', showHideWinLine);
-
-
-// This will go into the game object later
-winLine = document.querySelector('.win-line');
-function showHideWinLine() {
-    winLine.classList.toggle('visible');
-}
-moveWinLine('[1, 9]');
-function moveWinLine(coordinates) {
-    switch (coordinates) {
-        case '[1, 9]': // diagonal
-            lineTransform = 'rotate(45deg) translate(-148px, -148px)';
-            break;
-        case '[4, 6]': // middle row
-            lineTransform = 'translateY(-210px)';
-            break;
-        case '[1, 3]': // top row
-            lineTransform = 'translateY(-315px)';
-            break;
-        case '[7, 9]': // bottom row
-            lineTransform = 'translateY(-105px)';
-            break;
-        case '[3, 7]': // diagonal
-            lineTransform = 'rotate(135deg) translate(-148px, 148px)';
-            break;
-        case '[1, 7]': // first column
-            lineTransform = 'translate(-105px, -205px) rotate(90deg)';
-            break;
-        case '[2, 8]': // second column
-            lineTransform = 'translate(0, -205px) rotate(90deg)';
-            break;
-        case '[3, 9]': // third column
-            /* X and Y transform switch if you rotate first*/
-            lineTransform = 'rotate(90deg) translate(-205px, -105px)';
-            break;
-    }
-    winLine.style.transform = `${lineTransform}`;
-
-}
+game.addEventListeners();
